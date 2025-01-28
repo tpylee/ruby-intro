7228d3d7c555ad52c5ad5caa428a2aacdb93c2c7
tpylee Lee <tanya.lee@kellogg.northwestern.edu>
2025-01-28 20:36:39 +0000

    Module_2

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -4,6 +4,15 @@
 
 # Arrays are lists of things
 
+favoroute_foods = ["taco", "pizza", "ice cream"]
+puts favoroute_foods
+p favoroute_foods 
+
+mixed_arrays = ["tacos", 12, true]
+p mixed_arrays
+
+
+
 # Accessing the array
 
 # Add to the array
